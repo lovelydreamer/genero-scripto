d7fed75b6fa831b1846ed94c7402f82effa92c32
Christina <lovelydreamer@users.noreply.github.com>
2020-12-07 19:16:35 -0600

    dropping ideas in to come back to

--- getStarted.sh
+++ getStarted.sh
@@ -2,7 +2,7 @@
 # script to run when doing engagements
 # USE: $source get_started.script (allows for cd in bash)
 # 1st arg -- project name 
-# 2nd arg - target ip   
+# 2nd arg - TARGET_IP  
 # 3rd arg - if port or range
 
 
@@ -14,7 +14,7 @@ echo starting nmap
 #portChoice="USER INPUT"
 
 if [$3]; then
-        nmap -sC sV -F $2 $3 > nmap.txt
+        nmap -sC sV -F $2 $3 > nmap.txt # try string, ip has causing /n error
 else:
         nmap -SC -sV $2 > nmap.txt
 cat nmap.txt
@@ -26,3 +26,5 @@ cat nmap.txt
 # github dork
 # google dork
 
+# -w 
+# gobuster dir -u http://$TARGET_IP -w /usr/...wordlist
