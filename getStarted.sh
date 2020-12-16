3022109a4c1be48e88b526a617b318f0b1f1c009
Christina <lovelydreamer@users.noreply.github.com>
2020-12-16 12:20:55 -0600

    Update getStarted.sh

--- getStarted.sh
+++ getStarted.sh
@@ -4,27 +4,32 @@
 # 1st arg -- project name 
 # 2nd arg - TARGET_IP  
 # 3rd arg - if port or range
-
-
+NMAP = '/usr/bin/nmap'
+FILENAME=$1
+IP=$2
+#3rd arg - if port or range
 echo making folders
-mkdir /home/kali/Documents/$1
-cd /home/kali/Documents/$1/  
- 
+mkdir /home/kali/Documents/$FILENAME
+cd /home/kali/Documents/$FILENAME/ #do I need the trailing slash   
 echo starting nmap
-#portChoice="USER INPUT"
+ if [$IP]; then echo found param;
+# else
+# echo found no param;
+# echo end of program
 
-if [$3]; then
-        nmap -sC sV -F $2 $3 > nmap.txt # try string, ip has causing /n error
-else:
-        nmap -SC -sV $2 > nmap.txt
-cat nmap.txt
- 
+#portChoice="USER INPUT"
+#if [$3]; then
+#        nmap -sC sV -F $2 $3 > nmap.txt # try string, ip has causing /n error
+#else:
+#        nmap -SC -sV $2 > nmap.txt
+#cat nmap.txt
  
 # searchsploit
 # vulnhub
 # NIST CVE
 # github dork
 # google dork
+# maybe dirbuster
 
 # -w (web)
 # gobuster dir -u http://$TARGET_IP -w /usr/...wordlist
